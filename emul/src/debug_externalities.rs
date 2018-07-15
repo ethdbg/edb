e5a50342ebcf36b261f98a7e9874a706c0ec3178
Andrew Plaza <aplaza@liquidthink.net>
2018-07-14 20:26:17 -0400

    add commented code that works for delegate!

--- emul/src/debug_externalities.rs
+++ emul/src/debug_externalities.rs
@@ -1,13 +1,14 @@
 use delegate;
 use vm;
-use vm::Ext;
 use ethcore::externalities::{Externalities, OriginInfo, OutputPolicy};
 use ethcore::state::{Backend as StateBackend, State, Substate};
 use ethcore::machine::EthereumMachine as Machine;
 use ethcore::trace::{Tracer, VMTracer};
-use vm::{EnvInfo, Schedule, CreateContractAddress, CallType, MessageCallResult};
+use vm::{EnvInfo, Schedule, Ext, CreateContractAddress, CallType, MessageCallResult, ContractCreateResult, ReturnData};
 use evm::interpreter::Interpreter;
 use evm::CostType;
+use bytes::Bytes;
+use std::sync::Arc;
 use std::any::Any;
 use ethereum_types::{H256, U256, Address};
 use emulator::InterpreterSnapshots;
@@ -78,6 +79,74 @@ impl<'a, T: 'a, V: 'a, B: 'a> DebugExt<'a, T, V, B>
             snapshots: InterpreterSnapshots::new()
         }
     }
+    /*
+    delegate! {
+        target self.externalities {
+            fn storage_at(&self, key: &H256) -> vm::Result<H256>;
+            
+            fn set_storage(&mut self, key: H256, value:  H256) -> vm::Result<()>;
+
+            fn exists(&self, address: &Address) -> vm::Result<bool>;
+
+            fn exists_and_not_null(&self, address: &Address) -> vm::Result<bool>;
+
+            fn origin_balance(&self) -> vm::Result<U256>;
+
+            fn balance(&self, address: &Address) -> vm::Result<U256>;
+
+            fn blockhash(&mut self, number: &U256) -> H256;
+
+            fn create(&mut self, gas: &U256, value: &U256, code: &[u8], 
+                      address: CreateContractAddress) -> ContractCreateResult;
+
+            fn call(&mut self, 
+                    gas: &U256, 
+                    sender_address: &Address, 
+                    receive_address: &Address, 
+                    value: Option<U256>, 
+                    data: &[u8], 
+                    code_address: &Address, 
+                    output: &mut [u8], 
+                    call_type: CallType
+            ) -> MessageCallResult;
+
+            fn extcode(&self, address: &Address) -> vm::Result<Arc<Bytes>>;
+
+            fn extcodesize(&self, address: &Address) -> vm::Result<usize>;
+
+            fn log(&mut self, topics: Vec<H256>, data: &[u8]) -> vm::Result<()>;
+
+            fn ret(self, gas: &U256, data: &ReturnData, apply_state: bool) -> vm::Result<U256>;
+
+            fn suicide(&mut self, refund_address: &Address) -> vm::Result<()>;
+
+            fn schedule(&self) -> &Schedule;
+
+            fn env_info(&self) -> &EnvInfo;
+
+            fn depth(&self) -> usize;
+
+            fn inc_sstore_clears(&mut self);
+
+            fn trace_next_instruction(&mut self, 
+                                      pc: usize, 
+                                      instruction: u8, 
+                                      current_gas: U256
+            ) -> bool;
+
+            fn trace_prepare_execute(&mut self, pc: usize, instruction: u8, gas_cost: U256);
+
+            fn trace_executed(&mut self, 
+                              gas_used: U256, 
+                              stack_push: &[U256], 
+                              mem_diff: Option<(usize, &[u8])>, 
+                              store_diff: Option<(U256, U256)>
+            );
+
+            fn is_static(&self) -> bool;
+        }
+    }
+    */
 }
 
 impl<'a, T: 'a, V: 'a, B: 'a> vm::Ext for DebugExt<'a, T, V, B> {
