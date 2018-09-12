603ec86910e2d8b27555afe7ce635a70f97d4e7d
Andrew Plaza <aplaza@liquidthink.net>
2018-09-12 00:49:27 -0400

    move vyper to its own crate

--- vyper/src/binds.rs
+++ vyper/src/binds.rs
@@ -7,7 +7,7 @@ use std::{
 use pyo3::prelude::*;
 use super::err::VyError;
 use super::types::parse::AstValue;
-
+/*
 // TODO: use from_code instead of global vyper mod
 pub fn parse(code_file: PathBuf) -> VyError<()> {
     let mut code = String::new();
@@ -20,18 +20,30 @@ pub fn parse(code_file: PathBuf) -> VyError<()> {
     let parser = py.import("vyper.parser.parser")?;
     let res = parser.call1("parse", code.as_str())?;
     let ast: &PyList = res.try_into()?;
-    let item = ast.get_item(0);
-    info!("Item: {}", item);
-    /*let ast = ast.iter().map(|x| {
-        x.try_into().unwrap()
-    }).collect::<Vec<&PyList>>();*/
+    let mut rust_ast: Vec<PyDict> = Vec::new();
+    for x in ast.iter() {
+        let dict: PyObject = x.to_object(py).getattr(py, "__dict__")?;
+        // rust_ast.push(dict.cast_as(py)?.clone());
+        let dict: &PyDict = dict.cast_as(py)?;
+        info!("Dict: {:?}", dict);
+    }
     Ok(())
 }
+*/
 
-pub fn get_contracts_and_defs_and_globals(parsed: PyList) -> PyResult<()> {
-    unimplemented!(); 
+pub fn get_contracts_and_defs_and_globals(code_file: PathBuf) -> PyResult<()> {
+    let mut code = String::new();
+    let mut file = File::open(code_file.as_path())?;
+    info!("Read {} bytes from vyper code", file.read_to_string(&mut code)?);
+    let arb_list = vec![0, 1, 3];
+    let gil = Python::acquire_gil();
+    let py = gil.python();
+    let sys = py.import("sys");
+    let parser = py.import("vyper.parser.parser")?;
+    let res = parser.call1("parse", code.as_str())?;
+
 }
-/*
+
 pub fn parse_tree_to_lll(parsed_code: ?, code: &str, runtime: bool) -> ? {
     unimplemented!();
 }
--- vyper/src/types/parse.rs
+++ vyper/src/types/parse.rs
@@ -6,6 +6,8 @@ pub enum AstValue {
 }
 
 pub struct Annotation {
+    target: Name,
+    annotation: Call,
     simple: i32,
     lineno: usize, // line number should never be negative
     col_offset: i32,
@@ -14,10 +16,32 @@ pub struct Annotation {
 
 pub struct FunctionDef {
     name: String,
+    args: Arguments,
+    returns: Name,
     lineno: usize,
     col_offset: i32,
     source_code: String
 }
+
+pub struct Arguments {
+    
+
+}
+
+pub struct Name {
+    id: String,
+    ctx: Context,
+    linno: usize,
+    col_offset: i32,
+    source_code: String,
+}
+
+pub enum Context {
+    Load,
+    Store,
+    Del,
+}
+
 /*
 impl FromPyObject for Value {
         
