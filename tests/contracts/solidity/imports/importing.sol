f30cc13354981a379177b539aa70e6ed74395391
Andrew Plaza <aplaza@liquidthink.net>
2018-09-24 12:15:16 -0400

    removed some redundant abstracted components. Creating own code mapping mechanisms

---
+++ tests/contracts/solidity/imports/importing.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.0;
+// For testing imports
+
+contract SimpleStorageTwo {
+    uint storedData;
+
+    function set(uint x) {
+        storedData = x;
+    }
+
+    function get() constant returns (uint) {
+        return storedData;
+    }
+}
